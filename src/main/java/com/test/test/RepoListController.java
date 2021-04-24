307e1f76b261d21be16c5c508d811c27c98b04c1
baluk <you@example.com>
2021-04-25 01:31:36 +0200

    gitEndpointsWorking, TODO exception handling

--- src/main/java/com/test/test/RepoListController.java
+++ src/main/java/com/test/test/RepoListController.java
@@ -1,24 +1,60 @@
 package com.test.test;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+//import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class RepoListController {
-	private final AtomicLong counter = new AtomicLong();
+	// private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/repoList")
-	public RepoList repoList(@RequestParam(value = "user", defaultValue = "-") String name) {
-		return new RepoList(counter.incrementAndGet(), name);
+	public Repo[] repoList(@RequestParam(value = "user", defaultValue = "-") String user) {
+		return getRepoList(user);
 	}
 
 	@GetMapping("/starsSum")
-	public Stars starsSum(@RequestParam(value = "user", defaultValue = "-") String name) {
-		return new Stars(counter.incrementAndGet(), name);
+	public Stars starsSum(@RequestParam(value = "user", defaultValue = "-") String user) {
+		return getRepoStars(user);
 	}
 
+	private Stars getRepoStars(String user) {
+		final String urlTemplate = "https://api.github.com/users/%s/repos?page=%d";
+		RestTemplate restTemplate = new RestTemplate();
+		long summarize = 0;
+		int i = 1;
+		Stars[] repos;
+		do {
+			repos = restTemplate.getForObject(String.format(urlTemplate, user, i), Stars[].class);
+			for (Stars s : repos) {
+				summarize += s.getStargazers_count();
+			}
+			i++;
+		} while (repos.length != 0);
+		return new Stars(summarize);
+	}
 	
+	private Repo[] getRepoList(String user) {
+
+		final String urlTemplate = "https://api.github.com/users/%s/repos?page=%d";
+		RestTemplate restTemplate = new RestTemplate();
+		List list = new ArrayList();
+		Repo[] repos;
+		int i =1;
+		do {
+			repos = restTemplate.getForObject(String.format(urlTemplate, user, i), Repo[].class);
+			list.addAll(Arrays.asList(repos));     
+			i++;
+		} while (repos.length != 0);
+		Repo[] finalArray = (Repo[]) list.toArray();     
+		return finalArray;
+
+	}
+
 }
--- src/main/java/com/test/test/Stars.java
+++ src/main/java/com/test/test/Stars.java
@@ -1,32 +1,16 @@
 package com.test.test;
 
-import org.springframework.web.client.RestTemplate;
-
 public class Stars {
-	
-	private final long id;
-	private final String content;
-	
-	public Stars(long id, String user) {
-		super();
-		this.id = id;
-		this.content = getSum(user);
-	}
+	private final long stargazers_count;
 
-	public long getId() {
-		return id;
-	}
+public Stars() {
+	this.stargazers_count = 0;
+}
+public Stars(long stars) {
+	this.stargazers_count = stars;
+}
 
-	public String getContent() {
-		return content;
-	}
-	
-	private String getSum(String user) {
-
-		final String urlTemplate = "https://api.github.com/users/%s/repos";
-		RestTemplate restTemplate = new RestTemplate();
-
-		return restTemplate.getForObject(String.format(urlTemplate, user), String.class);
-	}
-	
+public long getStargazers_count() {
+	return stargazers_count;
+}
 }
