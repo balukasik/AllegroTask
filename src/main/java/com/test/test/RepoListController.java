2f2a4ec0317422763f9adee2e17a9eb00f4f061c
baluk <you@example.com>
2021-04-25 16:55:30 +0200

    Git endpoints done, TODO HTTP 400, RUNTIME ERROR

--- src/main/java/com/test/test/RepoListController.java
+++ src/main/java/com/test/test/RepoListController.java
@@ -3,8 +3,6 @@ package com.test.test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-//import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,51 +11,26 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class RepoListController {
-	// private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/repoList")
-	public List<Repo> repoList(@RequestParam(value = "user", defaultValue = "") String user) {
+	public List<Repo> repoList(@RequestParam(value = "user", defaultValue = "") String user)
+			throws UserNotFoundException {
 		if (user.length() == 0) {
-			return null;
+			throw new UserNotSpecifiedException(user);
 		}
 		try {
 			return getRepoList(user);
 		} catch (HttpClientErrorException e) {
-			return null;
-		}
-	}
-
-	@GetMapping("/starsSum")
-	public Stars starsSum(@RequestParam(value = "user", defaultValue = "") String user) {
-		if (user.length() == 0) {
-			return null;
-		}
-		try {
-			return getRepoStars(user);
-		} catch (HttpClientErrorException e) {
-			return null;
-		}
-	}
-
-	private Stars getRepoStars(String user) throws HttpClientErrorException {
-		final String urlTemplate = "https://api.github.com/users/%s/repos?page=%d";
-		RestTemplate restTemplate = new RestTemplate();
-		long summarize = 0;
-		int i = 1;
-		Stars[] repos;
-		do {
-			repos = restTemplate.getForObject(String.format(urlTemplate, user, i), Stars[].class);
-			for (Stars s : repos) {
-				summarize += s.getStargazers_count();
+			if(e.getResponseBodyAsString().contains("https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting")) {
+				throw new LimitExceededException("Too many request, check https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting");
 			}
-			i++;
-		} while (repos.length != 0);
-		return new Stars(summarize);
+			throw new UserNotFoundException(user);
+		}
 	}
 
 	private List<Repo> getRepoList(String user) throws HttpClientErrorException {
 
-		final String urlTemplate = "https://api.github.com/users/%s/repos?page=%d";
+		final String urlTemplate = "https://api.github.com/users/%s/repos?per_page=100&page=%d";
 		RestTemplate restTemplate = new RestTemplate();
 		List<Repo> list = new ArrayList<Repo>();
 		Repo[] repos;
--- src/main/java/com/test/test/Stars.java
+++ src/main/java/com/test/test/Stars.java
@@ -3,14 +3,15 @@ package com.test.test;
 public class Stars {
 	private final long stargazers_count;
 
-public Stars() {
-	this.stargazers_count = 0;
-}
-public Stars(long stars) {
-	this.stargazers_count = stars;
-}
+	public Stars() {
+		this.stargazers_count = 0;
+	}
 
-public long getStargazers_count() {
-	return stargazers_count;
-}
+	public Stars(long stars) {
+		this.stargazers_count = stars;
+	}
+
+	public long getStargazers_count() {
+		return stargazers_count;
+	}
 }
